a5b826eeb90c729a051cffa976325c5fe4505ef1
Alex <agonopol@gmail.com>
2017-08-26 18:08:29 +0200

    [ag] initial commit

---
+++ lib/loaddeps.m
@@ -0,0 +1,10 @@
+function loaddeps()
+    [current, ~, ~] = fileparts(mfilename('fullpath'));
+    addpath(fullfile(current, 'condense'));
+    addpath(fullfile(current, 'condense/quality'));
+    addpath(fullfile(current, 'condense/emd/matlab'));
+    addpath(fullfile(current, 'centrality'));
+    addpath(fullfile(current, 'sanky'));
+    addpath(fullfile(current, 'sanky', 'template'));
+    addpath(fullfile(current, 'rindex'));
+end
